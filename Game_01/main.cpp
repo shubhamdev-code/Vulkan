c79da4c4c86234790e32d3e55ee920f023364a29
Shubham <shubhamkrs@gmail.com>
2023-03-23 02:12:40 +0530

    initial

---
+++ Game_01/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <GLFW/glfw3.h>
+
+static const int WIDTH = 1280;
+static const int HEIGHT = 720;
+static const char* APP_NAME = "Game_01";
+
+int main()
+{
+    if (!glfwInit())
+        return 1;
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, APP_NAME, nullptr, nullptr);
+
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+
+    glfwTerminate();
+
+    return 0;
+}
+
